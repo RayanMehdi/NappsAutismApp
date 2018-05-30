ed9dd5117c22d628c91be8b8b8198fa8756eaabd
Unknown <johanlannier@gmail.com>
2018-05-30 15:27:53 +0200

    Tests firebase + start sending data to watch

--- NappsAutismWatchApp Extension/InterfaceController.swift
+++ NappsAutismWatchApp Extension/InterfaceController.swift
@@ -8,13 +8,29 @@
 
 import WatchKit
 import Foundation
-
+import WatchConnectivity
 
 class InterfaceController: WKInterfaceController {
+    @IBOutlet var taskTitle: WKInterfaceLabel!
+    
+    
+    
+    func showTask(data: String){
+        taskTitle.setText(data)
+    }
 
+    var watchSession: WCSession? {
+        didSet {
+            if let session = watchSession {
+                session.delegate = self
+                session.activate()
+            }
+        }
+    }
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
+        watchSession = WCSession.default
         // Configure interface objects here.
     }
     
@@ -28,4 +44,23 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+}
+
+extension InterfaceController: WCSessionDelegate {
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        print("Session activation did complete")
+    }
+    
+    func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String : Any]) {
+        print("watch received app context: ", applicationContext)
+        print(applicationContext)
+    }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        print("didReceiveMessage")
+        if let data = message["showTask"] as? String {
+            self.showTask(data: data)
+        }
+    }
 }
